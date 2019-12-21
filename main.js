4e64179c0bf53049e522b2c5dc118fa5689107fa
Reza Shoja <rezasj88@gmail.com>
2019-12-21 22:30:08 +0330

    project started

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  //   var red = Math.floor(Math.random() * 255);
+  //   var green = Math.floor(Math.random() * 255);
+  //   var blue = Math.floor(Math.random() * 255);
+  //   var randomRGBA = "rgba(" + red + "," + green + "," + blue + ", 1)";
+  const body = this.document.querySelector("body");
+  const div = this.document.createElement("div");
+
+  div.textContent = "";
+  body.appendChild(div);
+  //   body.style.background = randomRGBA;
+};
